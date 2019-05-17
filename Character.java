c8466019afcbc216c46f7a09ef46e19699e2ab13
Eric Lam <elam00@stuy.edu>
2019-05-17 11:37:23 -0400

    Changed Classes

--- Character.java
+++ Character.java
@@ -1,15 +1,3 @@
-import processing.core.PApplet;
-
-public class Character extends PApplet{
-	public static void main(String[] args){
-		PApplet.main("Main");
-	}
-
-	@Override
-	public void settings(){
-		size(500, 500);
-	}
-
-
+public class Character implements Renderable{
 
 }
--- Wall.java
+++ Wall.java
@@ -1,15 +1,3 @@
-import processing.core.PApplet;
-
-public class Main extends PApplet{
-	public static void main(String[] args){
-		PApplet.main("Main");
-	}
-
-	@Override
-	public void settings(){
-		size(500, 500);
-	}
-
-
+public class Main implements Renderable{
 
 }

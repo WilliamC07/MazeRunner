95d4739746d344de0d091a87757f64c0a853aa8f
Eric Lam <elam00@stuy.edu>
2019-05-17 11:41:00 -0400

    Completed Point Class

--- Point.java
+++ Point.java
@@ -1,2 +1,13 @@
 public class Point{
+    private float x,y;
+    public Point(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
 }

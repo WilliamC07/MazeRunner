c76378bdeb5d6e120cc7e6bf342175d8ce3d6d5c
Eric Lam <elam00@stuy.edu>
2019-05-17 11:32:23 -0400

    Character class

---
+++ Character.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+
+public class Character extends PApplet{
+	public static void main(String[] args){
+		PApplet.main("Main");
+	}
+
+	@Override
+	public void settings(){
+		size(500, 500);
+	}
+
+
+
+}

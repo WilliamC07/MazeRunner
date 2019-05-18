a34918d68768da43282f9a4a93c13465e98512f5
William Cao <williamcao999@gmail.com>
2019-05-17 22:58:31 -0400

    added Wall boilerplate

--- Wall.java
+++ Wall.java
@@ -1,3 +1,28 @@
-public class Main implements Renderable{
+/**
+ * A wall is just a line segment that the user cannot walk through
+ */
+public class Wall implements Renderable{
+    /**
+     * The first point of the wall.
+     */
+    private Point start;
+    /**
+     * The second point of the wall.
+     */
+    private Point end;
 
+    /**
+     * Constructor for Wall.
+     * @param start Start point of the wall.
+     * @param end End point of the wall.
+     */
+    public Wall(Point start, Point end){
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void render(){
+
+    }
 }

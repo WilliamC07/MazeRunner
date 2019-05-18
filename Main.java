727b55515f26af696d3423a6d0b33c72570ef026
William Cao <williamcao999@gmail.com>
2019-05-17 23:34:21 -0400

    use singleton design pattern

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 import processing.core.PApplet;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main extends PApplet{
-	private ArrayList<Renderable> renderables;
+	private Character character;
+	private List<Renderable> renderables;
+	/**
+	 * Singleton design pattern so we don't need to keep passing reference to this class around. We need
+	 * the draw methods inside PApplet (like ellipse(float, float, float, float))
+	 */
+	private static Main instance;
 
 	public static void main(String[] args){
 		PApplet.main("Main");
@@ -11,7 +20,9 @@ public class Main extends PApplet{
 
 	@Override
 	public void settings(){
+		instance = this;
 		size(1000, 500);
+		renderables = new ArrayList<>();
 	}
 
 	@Override
@@ -22,6 +33,6 @@ public class Main extends PApplet{
 	@Override
 	public void draw(){
 		fill(255);
-
+		renderables.forEach(Renderable::render);
 	}
 }

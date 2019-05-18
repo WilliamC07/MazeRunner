f1c2e0367322026bab9ab02d92f934bf5a0f9b0d
William Cao <williamcao999@gmail.com>
2019-05-18 19:53:45 -0400

    drawn ray to edges of the screen

--- Character.java
+++ Character.java
@@ -6,12 +6,20 @@ public class Character implements Renderable{
     private Point location;
     private List<Wall> walls;
     private List<Ray> rays;
+    private Point[] borderPoints;
 
     public Character(Point location, List<Wall> walls){
         this.location = location;
         // TODO: update when the Maze.java is finished. I have some testing code for now
         this.walls = walls;
         this.rays = new ArrayList<>();
+        Main main = Main.getInstance();
+        this.borderPoints = new Point[]{
+                new Point(0, 0),
+                new Point(0, main.height),
+                new Point(main.width, 0),
+                new Point(main.width, main.height)
+        };
     }
 
     public void move(Point newLocation){
@@ -27,7 +35,7 @@ public class Character implements Renderable{
 
     private void findIntersectionPoints(){
         // n^2 algorithm yikes
-
+        // find intersections for walls in the maze
         for(Wall wall : walls){
             // draw a ray to start and end point
             Ray toStart = new Ray(location, wall.getStart());
@@ -50,6 +58,18 @@ public class Character implements Renderable{
             }
             if(toEnd != null){
                 rays.add(toEnd);
+            }
+        }
+
+        for(Point borderPoint : borderPoints){
+            Ray toBorderPoint = new Ray(location, borderPoint);
+            for(Wall wallCheck : walls){
+                if(toBorderPoint != null && toBorderPoint.intersects(wallCheck) != null){
+                    toBorderPoint = null;
+                }
+            }
+            if(toBorderPoint != null){
+                rays.add(toBorderPoint);
             }
         }
     }

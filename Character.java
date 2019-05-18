f53607fbdf5c54d1ea2b272adbfdf7c7a903902e
William Cao <williamcao999@gmail.com>
2019-05-17 23:19:03 -0400

    added boiler plate code

--- Character.java
+++ Character.java
@@ -1,3 +1,23 @@
-public class Character implements Renderable{
+import java.util.List;
+import java.util.Set;
 
+public class Character implements Renderable{
+    private Point location;
+    private Set<Wall> walls;
+    private List<Point> intersectionPoints;
+
+    public Character(Point location, Set<Wall> walls){
+        this.location = location;
+        // TODO: update when the Maze.java is finished. I have some testing code for now
+        this.walls = walls;
+    }
+
+    public void move(Point newLocation){
+        this.location = newLocation;
+    }
+
+    @Override
+    public void render(){
+
+    }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class Main extends PApplet{
+	private ArrayList<Renderable> renderables;
+
 	public static void main(String[] args){
 		PApplet.main("Main");
 	}
 
 	@Override
 	public void settings(){
-		size(500, 500);
+		size(1000, 500);
 	}
 
+	@Override
+	public void setup(){
 
+	}
 
+	@Override
+	public void draw(){
+		fill(255);
+
+	}
 }

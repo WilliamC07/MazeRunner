b407593bd15a48ea0593532107df9627c021d607
William Cao <williamcao999@gmail.com>
2019-05-18 19:11:31 -0400

    wrote Ray boilerplate code

--- Ray.java
+++ Ray.java
@@ -1,3 +1,17 @@
 public class Ray{
+    private Point start;
+    private Point end;
+    private double slope;
+
+    /**
+     * Creates a ray given the start position and degree coming from the character
+     * @param start The character's location
+     * @param end The end of the line
+     */
+    public Ray(Point start, Point end){
+        this.start = start;
+        this.end = end;
+    }
+
 
 }

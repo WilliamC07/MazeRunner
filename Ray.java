1998f04b441b33ba01fcd66315e7526a18333c2f
William Cao <williamcao999@gmail.com>
2019-05-19 12:57:44 -0400

    refractor calculating intersection

--- Ray.java
+++ Ray.java
@@ -1,7 +1,6 @@
 public class Ray implements Renderable{
     private Point start;
     private Point end;
-    private double degreeOfSlope;
 
     /**
      * Creates a ray given the start position and end position from the character.
@@ -11,17 +10,17 @@ public class Ray implements Renderable{
     public Ray(Point start, Point end){
         this.start = start;
         this.end = end;
+
     }
 
     /**
-     * Creates a ray given the start position and the slope (delta x / delta y)
-     * @param start
-     * @param slope
+     * Creates a ray given the start position and the degree of slope
+     * @param start Start point of the ray
+     * @param degreeOfSlope The arctan of the slope is the degree of the line
      */
-    public Ray(Point start, double slope){
-        // the arctan of the slope is the degree of the line
+    public Ray(Point start, double degreeOfSlope){
         this.start = start;
-        this.degreeOfSlope = Math.atan(slope);
+
     }
 
     /**
@@ -31,12 +30,39 @@ public class Ray implements Renderable{
      * @return null if no intersection, a point instance if there is an intersection
      */
     public Point intersects(Wall wall){
-        if(end == null){
+        if(end == null) {
             throw new IllegalStateException("No end point determined");
         }
 
+        float[] result = lineLineIntersectionValues(wall);
+        // collinear lines means that we return the wall endpoint that is closest to the ray start point
+        if(result == null){
+            float distanceToWallStart = result[0];
+            float distanceToWallEnd = result[1];
+            return distanceToWallStart > distanceToWallEnd ? wall.getEnd() : wall.getStart();
+        }
+
+        float t = result[0];
+        float u = result[1];
+        if(t >= 0 && t <= 1 && u >= 0 && u <= 1){
+            // there is an intersection
+            float intersectX = (start.getX() + t * (end.getX() - start.getX()));
+            float intersectY = (start.getY() + t * (end.getY() - start.getY()));
+            return new Point(intersectX, intersectY);
+        }
+
+        // no intersection
+        return null;
+    }
+
+    /**
+     * Performs the line-line intersection algorithm.
+     * Algorithm: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+     * @param wall Wall to check if there is an intersection
+     * @return {t, u} if there is an intersection. {distanceToWallStart, distanceToWallEnd} if the lines are collinear.
+     */
+    private float[] lineLineIntersectionValues(Wall wall){
         // Write out all of this so there is no mistake when implementing the formula
-        // Algorithm: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
         float x1 = start.getX();
         float x2 = end.getX();
         float x3 = wall.getStart().getX();
@@ -47,24 +73,18 @@ public class Ray implements Renderable{
         float y3 = wall.getStart().getY();
         float y4 = wall.getEnd().getY();
 
-        float demoninator = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-        if(demoninator == 0){
-            // Causes division by 0 the line segments are collinear. Return the closest point
-            double distanceToWallEnd = start.distancePortion(wall.getEnd());
-            double distanceToWallStart = start.distancePortion(wall.getStart());
-            return distanceToWallEnd > distanceToWallStart ? wall.getStart() : wall.getEnd();
+        float denominator = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+        if(denominator == 0){
+            // Causes division by 0 which means line segments are collinear.
+            // Return null since we can no longer calculate the t and u values
+            float distanceToWallStart = (float) start.distancePortion(wall.getStart());
+            float distanceToWallEnd = (float) start.distancePortion(wall.getEnd());
+            return new float[]{distanceToWallStart, distanceToWallEnd};
         }
 
-        float t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / demoninator;
-        float u = -1 * ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / demoninator;
-        if(t >= 0 && t <= 1 && u >= 0 && u <= 1){
-            // there is an intersection
-            float intersectX = (x1 + t * (x2 - x1));
-            float intersectY = (y1 + t * (y2 - y1));
-            return new Point(intersectX, intersectY);
-        }else{
-            return null;
-        }
+        float t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denominator;
+        float u = -1 * ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denominator;
+        return new float[]{t, u};
     }
 
     @Override

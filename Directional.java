755cb777dc1ced68f7fa5130715236f1174d4eef
William Cao <williamcao999@gmail.com>
2019-05-21 21:18:36 -0400

    wrote direction enums

---
+++ Directional.java
@@ -0,0 +1,13 @@
+public class Directional {
+    public static enum Horizontal{
+        LEFT,
+        RIGHT,
+        NONE
+    }
+
+    public static enum Vertical{
+        TOP,
+        BOTTOM,
+        NONE
+    }
+}

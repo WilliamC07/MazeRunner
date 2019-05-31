e76674ded3975b719a3239fd0104a4d40705380d
Eric Lam <elam00@stuy.edu>
2019-05-31 11:36:55 -0400

    Borders Now Block Character

--- Character.java
+++ Character.java
@@ -1,3 +1,4 @@
+import processing.core.PApplet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,12 +13,12 @@ public class Character implements Renderable{
 
     public Character(Point location, List<Wall> walls){
         this.location = location;
+        sketch = Main.getInstance();
         // TODO: update when the Maze.java is finished. I have some testing code for now
-        Main main = sketch;
         Point a = new Point(1, 1);
-        Point b = new Point(main.width - 2, 1);
-        Point c = new Point(main.width - 2, main.height - 2);
-        Point d = new Point(1, main.height - 2);
+        Point b = new Point(sketch.width - 2, 1);
+        Point c = new Point(sketch.width - 2, sketch.height - 2);
+        Point d = new Point(1, sketch.height - 2);
         this.borderWall = new Wall[]{
                 new Wall(a, b),
                 new Wall(a, d),
@@ -28,7 +29,6 @@ public class Character implements Renderable{
         for(Wall border : borderWall){
             allWalls.add(border);
         }
-        sketch = Main.getInstance();
     }
 
     public void move(){
@@ -47,7 +47,7 @@ public class Character implements Renderable{
             dy+=1;
         }
         float newX = sketch.constrain(location.getX()+3f*dx,sketch.width/10f+10,9*sketch.width/10f-10);
-        float newY = sketch.constrain(location.getY()+3f*dy,sketch.height/10f+10,9*sketch.height/10f-10)
+        float newY = sketch.constrain(location.getY()+3f*dy,sketch.height/10f+10,9*sketch.height/10f-10);
         location = new Point(newX,newY);
     }
 
@@ -93,18 +93,18 @@ public class Character implements Renderable{
 
         // Draw a ray to each endpoint if there is not a wall blocking the way
         for(Wall wall : allWalls){
-            Ray mainRayStart = new Ray(location, wall.getStart(), true, wall);
-            Ray mainRayEnd = new Ray(location, wall.getEnd(), true, wall);
+            Ray sketchRayStart = new Ray(location, wall.getStart(), true, wall);
+            Ray sketchRayEnd = new Ray(location, wall.getEnd(), true, wall);
 
             // If a ray cannot be drawn, do not keep track of it
             // If the ray can be drawn, keep track of it and generate the auxiliary ray
-            if(!isMainRayBlocked(mainRayStart, wall)){
-                rays.add(mainRayStart);
-                mainRayStart.setAuxiliaryRay(createAuxiliaryRay(mainRayStart));
+            if(!issketchRayBlocked(sketchRayStart, wall)){
+                rays.add(sketchRayStart);
+                sketchRayStart.setAuxiliaryRay(createAuxiliaryRay(sketchRayStart));
             }
-            if(!isMainRayBlocked(mainRayEnd, wall)){
-                rays.add(mainRayEnd);
-                mainRayEnd.setAuxiliaryRay(createAuxiliaryRay(mainRayEnd));
+            if(!issketchRayBlocked(sketchRayEnd, wall)){
+                rays.add(sketchRayEnd);
+                sketchRayEnd.setAuxiliaryRay(createAuxiliaryRay(sketchRayEnd));
             }
         }
 
@@ -128,7 +128,7 @@ public class Character implements Renderable{
             }
 
             if(shareWall){
-                // if the ray is drawing to the same wall, then use the main lines to connect
+                // if the ray is drawing to the same wall, then use the sketch lines to connect
                 sketch.triangle(location.getX(), location.getY(),
                                             current.getEnd().getX(), current.getEnd().getY(),
                                             next.getEnd().getX(), next.getEnd().getY());
@@ -138,7 +138,7 @@ public class Character implements Renderable{
 
                 // The rays are not drawn to the same point
                 // If a ray cannot be drawn to the midpoint between the two auxiliary rays, we must connect
-                // auxiliary to main
+                // auxiliary to sketch
 
                 Point midpointAuxAux = Point.midpoint(currentAuxiliary.getEnd(), nextAuxiliary.getEnd());
                 boolean isMidpointBlocked = false;
@@ -156,19 +156,19 @@ public class Character implements Renderable{
                 if(isMidpointBlocked){
                     // Cannot connect the aux
 
-                    boolean canDrawMainAux = true;
-                    // if the main cannot be drawn to the aux midpoint without intersecting a wall at another point other
-                    // than the start of the main ray or the aux endpoint, it is not valid
+                    boolean canDrawsketchAux = true;
+                    // if the sketch cannot be drawn to the aux midpoint without intersecting a wall at another point other
+                    // than the start of the sketch ray or the aux endpoint, it is not valid
                     Ray checkBlocking = new Ray(current.getEnd(), nextAuxiliary.getEnd(), true, null);
                     for(Wall block : allWalls){
                         Point intersection = checkBlocking.intersects(block);
                         if(intersection != null && !intersection.equals(current.getEnd()) && !intersection.equals(nextAuxiliary.getEnd())){
-                            canDrawMainAux = false;
+                            canDrawsketchAux = false;
                             break;
                         }
                     }
 
-                    if(canDrawMainAux){
+                    if(canDrawsketchAux){
                         sketch.triangle(location.getX(), location.getY(),
                                 current.getEnd().getX(), current.getEnd().getY(),
                                 nextAuxiliary.getEnd().getX(), nextAuxiliary.getEnd().getY());
@@ -194,7 +194,7 @@ public class Character implements Renderable{
      * @param wallToTouch Wall the ray is attempting to collide with. Null if the ray is pointing to a point
      * @return True if the ray is blocked, false otherwise
      */
-    private boolean isMainRayBlocked(Ray ray, Wall wallToTouch){
+    private boolean issketchRayBlocked(Ray ray, Wall wallToTouch){
         for(Wall wall : allWalls){
             // The ray will always touch the wall it is aiming to touch
             if(wall.equals(wallToTouch)){
@@ -211,15 +211,15 @@ public class Character implements Renderable{
         return false;
     }
 
-    private Ray createAuxiliaryRay(Ray mainRay){
+    private Ray createAuxiliaryRay(Ray sketchRay){
         // if the may ray is drawn to the border wall, the auxiliary ray is itself
         for(Wall borderWall : borderWall){
-            if(borderWall.isPointOnWall(mainRay.getEnd())){
-                return mainRay;
+            if(borderWall.isPointOnWall(sketchRay.getEnd())){
+                return sketchRay;
             }
         }
-        // mainRay.getEnd() gives direction of the geometric ray
-        Ray auxiliary = new Ray(mainRay.getStart(), mainRay.getEnd(), false, null);
+        // sketchRay.getEnd() gives direction of the geometric ray
+        Ray auxiliary = new Ray(sketchRay.getStart(), sketchRay.getEnd(), false, null);
         for(Wall collideWall : allWalls){
             Point collisionPoint = auxiliary.intersects(collideWall);
             // make sure there is an intersection and the collision point is not the directional point (since directional
@@ -252,9 +252,9 @@ public class Character implements Renderable{
             }
 
             if(!isAuxBlocked){
-                return new Ray(mainRay.getStart(), collisionPoint, true, collideWall);
+                return new Ray(sketchRay.getStart(), collisionPoint, true, collideWall);
             }
         }
-        return mainRay;
+        return sketchRay;
     }
 }

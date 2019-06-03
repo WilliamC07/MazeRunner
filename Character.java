5c7a6a2430da624679c148efb3db4ed73b7c7db0
Eric Lam <elam00@stuy.edu>
2019-06-02 20:34:59 -0400

    Fixed Bug w/ Diagonal Lines in Minimap

--- Character.java
+++ Character.java
@@ -60,7 +60,7 @@ public class Character implements Renderable{
         locationInMatrix = new Point(locationInMatrix.getX()+scalar*dx, locationInMatrix.getY()+scalar*dy);
         int[] currentCell = maze.getMatrixPoint(locationInMatrix);
         int[] previousCell = maze.getPathKeeper();
-        if(currentCell[0]/2!=previousCell[0] || currentCell[1]/2!=previousCell[1]){
+        if(Math.abs(currentCell[0]/2-previousCell[0])==1 ^ Math.abs(currentCell[1]/2-previousCell[1])==1){
             maze.updateCell(currentCell[0]/2,currentCell[1]/2);
         }
     }

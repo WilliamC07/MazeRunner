673f9c29b5121e6930894e68c58dcade58050ac6
William Cao <williamcao999@gmail.com>
2019-06-03 11:35:57 -0400

    added decal to play section

--- TitleScreen.java
+++ TitleScreen.java
@@ -4,13 +4,19 @@ public class TitleScreen implements Renderable{
     private StringBuilder stringBuilder = new StringBuilder();
     private final Main main = Main.getInstance();
     private Mode mode;
-    private final PImage titleImage, whatIsThisImage, playImage;
     private final float centerX = main.width / 2;
+
+    // title screen variables
+    private final PImage titleImage, whatIsThisImage, playImage;
     private float titleImageY = .2F * main.height;
     private float whatIsThisButtonY = .40F * main.height;
     private float playButtonY = .60F * main.height;
     private final float widthTitleButton = .5F * main.width; // 50% of width
     private final float heightTitleButton = .1F * main.height; // 10% of width
+
+    // play screen variables
+    private final PImage widthImage, heightImage, monsterImage;
+    private PlayField playField;
 
     public TitleScreen(){
         mode = Mode.TITLE;
@@ -20,6 +26,12 @@ public class TitleScreen implements Renderable{
         whatIsThisImage.resize(400, 0);
         playImage = main.loadImage("play.png");
         playImage.resize(200, 0);
+        widthImage = main.loadImage("width.png");
+        widthImage.resize(400, 0);
+        heightImage = main.loadImage("height.png");
+        heightImage.resize(400, 0);
+        monsterImage = main.loadImage("monsters.png");
+        monsterImage.resize(350, 0);
     }
 
     @Override
@@ -27,6 +39,9 @@ public class TitleScreen implements Renderable{
         switch(mode){
             case TITLE:
                 renderTitleMode();
+                break;
+            case PLAY:
+                renderPlayMode();
                 break;
         }
     }
@@ -43,6 +58,24 @@ public class TitleScreen implements Renderable{
         main.image(playImage, centerX, playButtonY);
     }
 
+    private void renderPlayMode(){
+        main.imageMode(main.CENTER);
+        main.rectMode(main.CENTER);
+
+        float leftX = .2F * main.width;
+        float widthY = .1F * main.height;
+        float heightY = .3F * main.height;
+        float monstersY = .5F * main.height;
+        float playY = .7F * main.height;
+
+        main.image(widthImage, leftX, widthY);
+        main.image(heightImage, leftX, heightY);
+        main.image(monsterImage, leftX, monstersY);
+        main.fill(33, 136, 56);
+        main.rect(centerX, playY, widthTitleButton, heightTitleButton);
+        main.image(playImage, centerX, playY);
+    }
+
     public void feedCharacter(char key){
 
     }
@@ -53,11 +86,11 @@ public class TitleScreen implements Renderable{
                 // what is this section clicked
                 if(main.mouseX >= centerX - widthTitleButton / 2 && main.mouseX <= centerX + widthTitleButton / 2 &&
                         main.mouseY >= whatIsThisButtonY - heightTitleButton / 2 && main.mouseY <= whatIsThisButtonY + heightTitleButton / 2){
-                    System.out.println("clicked what is this section");
+                    mode = Mode.WHAT_IS_THIS;
                 }
                 if(main.mouseX >= centerX - widthTitleButton / 2 && main.mouseX <= centerX + widthTitleButton / 2 &&
                         main.mouseY >= playButtonY - heightTitleButton / 2 && main.mouseY <= playButtonY + heightTitleButton / 2){
-                    System.out.println("clicked play section");
+                    mode = Mode.PLAY;
                 }
                 break;
             case PLAY:
@@ -73,5 +106,11 @@ public class TitleScreen implements Renderable{
         TITLE,
         WHAT_IS_THIS,
         PLAY
+    }
+
+    private enum PlayField{
+        HEIGHT,
+        WIDTH,
+        MONSTER
     }
 }

95cf45b0586add823939588613b746eb053a4424
William Cao <williamcao999@gmail.com>
2019-06-03 21:35:59 -0400

    added distance back from old commit

--- Point.java
+++ Point.java
@@ -49,6 +49,18 @@ public class Point{
         return new Point((a.x + b.x) / 2, (a.y + b.y) / 2);
     }
 
+    /**
+     * Performs the square of the distance between points. This is to avoid doing the useless calculation
+     * of sqrt, which is expensive.
+     *
+     * d^2 = (x1 - x2)^2 + (y1 - y2)^2
+     * Note that the "^" means exponent and not the exclusive or symbol in java
+     * @return Distance between this point and the other point. This does not calculate square root
+     */
+    public double distancePortion(Point other){
+        return Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
